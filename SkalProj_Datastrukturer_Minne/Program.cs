d906eeb91a7b79c410c4498e5c4ae93c5286ddf7
IronPikachu <simon.pikachu@hotmail.se>
2021-12-13 11:28:38 +0100

    Excersizes 1-4 finished and answered, ReverseText() is not tested however...

--- SkalProj_Datastrukturer_Minne/Program.cs
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace SkalProj_Datastrukturer_Minne
 {
@@ -78,6 +79,51 @@ namespace SkalProj_Datastrukturer_Minne
             //string value = input.substring(1);
 
             //switch(nav){...}
+
+            List<string> strings = new List<string>();
+            bool examine = true;
+            const char exitCondition = '!';
+            string menu = $"Type '{exitCondition}' to exit\n'+' To add strings to the list\n'-'To remove matching strings from the list\n";
+
+            Console.Write("Hello! ");
+            while (examine)
+            {
+                Console.WriteLine(menu);
+                string? input = Console.ReadLine();
+                input ??= "BAD INPUT";
+                switch (input[0])
+                {
+                    case '+':
+                        //Insert input.substring(1)
+                        strings.Add(input.Substring(1));
+                        Console.WriteLine($"Count: {strings.Count}, Capacity: {strings.Capacity}");
+                        break;
+                    case '-':
+                        //Remove input.substring(1)
+                        strings.Remove(input.Substring(1));
+                        Console.WriteLine($"Count: {strings.Count}, Capacity: {strings.Capacity}");
+                        break;
+                    case exitCondition:
+                        examine = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid input...");
+                        break;
+                }
+            }
+            /*
+            1.Skriv klart implementationen av ExamineList-metoden så att undersökningen blir genomförbar. ✔
+            2.När ökar listans kapacitet ? (Alltså den underliggande arrayens storlek)
+                När man försöker lägga in ett till element och Capacity == Count
+            3.Med hur mycket ökar kapaciteten?
+                Dubblar (capacity *= 2)
+            4.Varför ökar inte listans kapacitet i samma takt som element läggs till ?
+                Mer tidseffektivt, ivartfall om den funkar som C++ std::vector, vilket den verkar göra
+            5.Minskar kapaciteten när element tas bort ur listan?
+                Nej
+            6.När är det då fördelaktigt att använda en egendefinierad array istället för en lista ?
+                När man själv vill bestämma allt i arrayen
+            */
         }
 
         /// <summary>
@@ -90,6 +136,48 @@ namespace SkalProj_Datastrukturer_Minne
              * Create a switch with cases to enqueue items or dequeue items
              * Make sure to look at the queue after Enqueueing and Dequeueing to see how it behaves
             */
+
+            //Que: FIFO -> deque remove first element, enque add to the back
+            Queue<string> strings = new Queue<string>();
+            bool examine = true;
+            const char exitCondition = '!';
+            string menu = $"Type '{exitCondition}' to exit\n'+' To add strings to the list\n'-'To remove matching strings from the list\n";
+
+            Console.Write("Hello! ");
+            while (examine)
+            {
+                Console.WriteLine(menu);
+                string? input = Console.ReadLine();
+                input ??= "BAD INPUT";
+                switch (input[0])
+                {
+                    case '+':
+                        //Insert input.substring(1)
+                        strings.Enqueue(input.Substring(1));
+                        foreach (var item in strings)
+                        {
+                            Console.Write($"{item} ");
+                        }
+                        Console.WriteLine();
+                        break;
+                    case '-':
+                        //Remove input.substring(1)
+                        if(strings.Count > 0)
+                            strings.Dequeue();
+                        foreach (var item in strings)
+                        {
+                            Console.Write($"{item} ");
+                        }
+                        Console.WriteLine();
+                        break;
+                    case exitCondition:
+                        examine = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid input...");
+                        break;
+                }
+            }
         }
 
         /// <summary>
@@ -102,6 +190,71 @@ namespace SkalProj_Datastrukturer_Minne
              * Create a switch with cases to push or pop items
              * Make sure to look at the stack after pushing and and poping to see how it behaves
             */
+
+            //Stack: LIFO -> push add to first element, pop remove first element
+            Stack<string> strings = new Stack<string>();
+            bool examine = true;
+            const char exitCondition = '!';
+            string menu = $"Hello! Type '{exitCondition}' to exit\n'+' To add strings to the list\n'-'To remove matching strings from the list\n";
+
+            while (examine)
+            {
+                Console.WriteLine(menu);
+                string? input = Console.ReadLine();
+                input ??= "BAD INPUT";
+                switch (input[0])
+                {
+                    case '+':
+                        //Insert input.substring(1)
+                        strings.Push(input.Substring(1));
+                        foreach (var item in strings)
+                        {
+                            Console.Write($"{item} ");
+                        }
+                        Console.WriteLine();
+                        break;
+                    case '-':
+                        //Remove input.substring(1)
+                        if (strings.Count > 0)
+                            strings.Pop();
+                        foreach (var item in strings)
+                        {
+                            Console.Write($"{item} ");
+                        }
+                        Console.WriteLine();
+                        break;
+                    case exitCondition:
+                        examine = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid input...");
+                        break;
+                }
+            }
+            /*
+            1. Simulera ännu en gång ICA-kön på papper. Denna gång med en stack . Varför är det inte så smart att använda en stack i det här fallet?
+                Det är orättvist, ingen skulle vilja vara först i en stack att betala på ica såvida man inte är ensam kund i butiken. Annars är det detsamma som att låta alla andra gå före i kön tills man är sist ungefär (ivartfall om man är först i kön).
+            2. Implementera en ReverseText-metod som läser in en sträng från användaren och med hjälp av en stack vänder ordning på teckenföljden för att sedan skriva ut den omvända strängen till användaren.
+                ✔
+             */
+        }
+
+        static void ReverseText()
+        {
+            Stack<char> stack = new Stack<char>();
+            string? input = Console.ReadLine();
+            input ??= " ";
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+            }
+            StringBuilder sb = new StringBuilder();
+            while(stack.Count > 0)
+            {
+                sb.Append(stack.Pop());
+            }
+            string output = sb.ToString();
+            Console.WriteLine(output);
         }
 
         static void CheckParanthesis()
@@ -112,8 +265,63 @@ namespace SkalProj_Datastrukturer_Minne
              * Example of incorrect: (()]), [), {[()}],  List<int> list = new List<int>() { 1, 2, 3, 4 );
              */
 
-        }
+            string? input = Console.ReadLine();
+            input ??= " ";
+            Stack<char> chars = new Stack<char>();
+            bool correctness = true;
 
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch (input[i])
+                {
+                    case '(':
+                        chars.Push('(');
+                        break;
+                    case '{':
+                        chars.Push('{');
+                        break;
+                    case '[':
+                        chars.Push('[');
+                        break;
+                    case ')':
+                    case '}':
+                    case ']':
+                        char output = chars.Pop();
+                        switch (output)
+                        {
+                            case '(':
+                                if (input[i] != ')')
+                                    correctness = false;
+                                break;
+                            case '{':
+                                if (input[i] != '}')
+                                    correctness = false;
+                                break;
+                            case '[':
+                                if (input[i] != ']')
+                                    correctness = false;
+                                break;
+                        }
+
+                        break;
+                    default:
+                        //Do nothing
+                        break;
+                }
+            }
+
+            if(correctness && chars.Count == 0)
+                Console.WriteLine("Correct formatting!");
+            else
+                Console.WriteLine("Incorrect formatting...");
+
+            /*
+            1. Skapa med hjälp av er nya kunskap funktionalitet för att kontrollera en välformad sträng på papper. Du ska använda dig av någon eller några av de datastrukturer vi precis gått igenom. Vilken datastruktur använder du?
+                Stack. Pusha (, { och [, och poppa när man stöter på ), } eller ]. Jämför sedan det som gav utfall för poppningen ifall den är dess 'partner'. Kolla också att stacken är tom i slutet. 
+            2. Implementera funktionaliteten i metoden CheckParantheses . Låt programmet läsa in en sträng från användaren och returnera ett svar som reflekterar huruvida strängen är välformad eller ej.
+                ✔
+             */
+        }
     }
 }
 
